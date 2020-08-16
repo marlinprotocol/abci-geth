fa3d45363c80900b2feac97a198517c486d21e44
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-08-16 13:16:11 +0530

    Implement block spam check rpc

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -136,7 +136,7 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 			Namespace: "lin",
 			Version:   "1.0",
 			Service:   NewMarlinAPI(apiBackend),
-			Public:    false,
+			Public:    true,
 		},
 	}
 }
